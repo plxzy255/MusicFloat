881d9dc24ddc15e2b01701db006f7f9a51c10bba
plxzy255 <29236149+plxzy255@users.noreply.github.com>
2026-05-23 22:13:55 +0200

    Initial Commit

---
+++ MusicFloat/MusicFloatApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicFloatApp.swift
+//  MusicFloat
+//
+//  Created by psp on 23/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct MusicFloatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
